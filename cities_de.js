2f965be69dd3dd642a4f788d81855eb66f7c2890
oliverheilig <oliver.heilig@yahoo.de>
2013-11-03 20:50:51 +0100

    added bawue cities

--- cities_de.js
+++ cities_de.js
@@ -23,6 +23,158 @@
 	},
 	"type": "Feature",
 	"id": "Stuttgart"
+	},
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [9.479167, 47.654167]
+	},
+	"type": "Feature",
+	"id": "Friedrichshafen"
+	},
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [9.613611, 47.781944]
+	},
+	"type": "Feature",
+	"id": "Ravensburg"
+	},
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [9.644444, 47.809167]
+	},
+	"type": "Feature",
+	"id": "Weingarten (Württemberg)"
+	},
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [8.71, 49.412222]
+	},
+	"type": "Feature",
+	"id": "Heidelberg"
+	},
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [9.222222, 49.141667]
+	},
+	"type": "Feature",
+	"id": "Heilbronn"
+	},
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [9.175278, 47.663333]
+	},
+	"type": "Feature",
+	"id": "Konstanz"
+	},
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [7.661389, 47.615556]
+	},
+	"type": "Feature",
+	"id": "Lörrach"
+	},
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [7.629444, 47.594167]
+	},
+	"type": "Feature",
+	"id": "Weil am Rhein"
+	},
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [8.463056,  49.483611]
+	},
+	"type": "Feature",
+	"id": "Mannheim"
+	},
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [8.447778,  49.483056]
+	},
+	"type": "Feature",
+	"id": "Ludwigshafen am Rhein"
+	},
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [7.940833,  48.470833]
+	},
+	"type": "Feature",
+	"id": "Offenburg"
+	},
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [8.698889,  48.891111]
+	},
+	"type": "Feature",
+	"id": "Pforzheim"
+	},
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [9.055556,  48.52]
+	},
+	"type": "Feature",
+	"id": "Tübingen"
+	},
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [9.216667,  48.483333]
+	},
+	"type": "Feature",
+	"id": "Reutlingen"
+	},
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [9.987222,  48.400833]
+	},
+	"type": "Feature",
+	"id": "Ulm"
+	},
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [8.491667,  48.056944]
+	},
+	"type": "Feature",
+	"id": "Villingen-Schwenningen"
+	},
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [9.7375,  49.112222]
+	},
+	"type": "Feature",
+	"id": "Schwäbisch Hall"
+	},
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [8.240833,  48.761944]
+	},
+	"type": "Feature",
+	"id": "Baden-Baden"
+	},
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [9.214722, 48.086389]
+	},
+	"type": "Feature",
+	"id": "Sigmaringen"
 	}
-	]
+]
 };
