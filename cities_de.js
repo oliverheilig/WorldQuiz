61a3edc9b10ae2110f8d6effd5db4cff41d623d6
oliverheilig <oliver.heilig@yahoo.de>
2013-11-04 22:39:27 +0100

    added bavarian cities

--- cities_de.js
+++ cities_de.js
@@ -175,6 +175,270 @@
 	},
 	"type": "Feature",
 	"id": "Sigmaringen"
+	},
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [11.848333,  49.444444]
+	},
+	"type": "Feature",
+	"id": "Amberg"
+	},
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [10.571667,  49.300833]
+	},
+	"type": "Feature",
+	"id": "Ansbach"
+	},
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [9.147747,  49.976703]
+	},
+	"type": "Feature",
+	"id": "Aschaffenburg"
+	},
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [10.898333,  48.371667]
+	},
+	"type": "Feature",
+	"id": "Augsburg"
+	},
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [10.891667,  49.891667]
+	},
+	"type": "Feature",
+	"id": "Bamberg"
+	},
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [11.578333,  49.948056]
+	},
+	"type": "Feature",
+	"id": "Bayreuth"
+	},
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [10.966667,  50.266667]
+	},
+	"type": "Feature",
+	"id": "Coburg"
+	},
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [12.964444,  48.835278]
+	},
+	"type": "Feature",
+	"id": "Deggendorf"
+	},
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [11.748889,  48.402778]
+	},
+	"type": "Feature",
+	"id": "Freising"
+	},
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [11.105278,  47.493889]
+	},
+	"type": "Feature",
+	"id": "Garmisch-Partenkirchen"
+	},
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [11.918889,  50.321944]
+	},
+	"type": "Feature",
+	"id": "Hof (Saale)"
+	},
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [11.424444,  48.765556]
+	},
+	"type": "Feature",
+	"id": "Ingolstadt"
+	},
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [10.624167,  47.881111]
+	},
+	"type": "Feature",
+	"id": "Kaufbeuren"
+	},
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [10.316667,  47.733333]
+	},
+	"type": "Feature",
+	"id": "Kempten (Allgäu)"
+	},
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [11.455556,  50.108056]
+	},
+	"type": "Feature",
+	"id": "Kulmbach"
+	},
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [12.152222,  48.537222]
+	},
+	"type": "Feature",
+	"id": "Landshut"
+	},
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [12.083056,  50.003056]
+	},
+	"type": "Feature",
+	"id": "Marktredwitz"
+	},
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [10.181111,  47.987778]
+	},
+	"type": "Feature",
+	"id": "Memmingen"
+	},
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [11.575556,  48.137222]
+	},
+	"type": "Feature",
+	"id": "München"
+	},
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [11.466667,  49.283333]
+	},
+	"type": "Feature",
+	"id": "Neumarkt in der Oberpfalz"
+	},
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [9.998889,  48.394167]
+	},
+	"type": "Feature",
+	"id": "Neu-Ulm"
+	},
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [11.077778,  49.452778]
+	},
+	"type": "Feature",
+	"id": "Nürnberg"
+	},
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [11.004311,  49.596361]
+	},
+	"type": "Feature",
+	"id": "Erlangen"
+	},
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [10.990278,  49.478333]
+	},
+	"type": "Feature",
+	"id": "Fürth"
+	},
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [11.020833,  49.329167]
+	},
+	"type": "Feature",
+	"id": "Schwabach"
+	},
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [13.456944,  48.573333]
+	},
+	"type": "Feature",
+	"id": "Passau"
+	},
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [12.096944,  49.017222]
+	},
+	"type": "Feature",
+	"id": "Regensburg"
+	},
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [12.128889,  47.856111]
+	},
+	"type": "Feature",
+	"id": "Rosenheim"
+	},
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [10.231667,  50.045556]
+	},
+	"type": "Feature",
+	"id": "Schweinfurt"
+	},
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [12.566667,  48.883333]
+	},
+	"type": "Feature",
+	"id": "Straubing"
+	},
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [12.648889,  47.871667]
+	},
+	"type": "Feature",
+	"id": "Traunstein"
+	},
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [12.15,  49.666667]
+	},
+	"type": "Feature",
+	"id": "Weiden in der Oberpfalz"
+	},
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [9.929444,  49.794444]
+	},
+	"type": "Feature",
+	"id": "Würzburg"
 	}
 ]
 };
