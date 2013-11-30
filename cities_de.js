e2b94f953628523c420629e4793bb337cbb42793
HoytPollard <oliver.heilig@yahoo.de>
2013-11-30 01:16:40 +0100

    completed german cities

--- cities_de.js
+++ cities_de.js
@@ -439,6 +439,806 @@
 	},
 	"type": "Feature",
 	"id": "Würzburg"
+	},
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [13.408056, 52.518611]
+	},
+	"type": "Feature",
+	"id": "Berlin"
+	},
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [12.563056, 52.408889]
+	},
+	"type": "Feature",
+	"id": "Brandenburg an der Havel"
+	},
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [14.334167, 51.760556]
+	},
+	"type": "Feature",
+	"id": "Cottbus"
+	},
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [14.550833, 52.341667]
+	},
+	"type": "Feature",
+	"id": "Frankfurt (Oder)"
+	},
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [13.061389, 52.395833]
+	},
+	"type": "Feature",
+	"id": "Potsdam"
+	},
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [8.807311, 53.075878]
+	},
+	"type": "Feature",
+	"id": "Bremen"
+	},
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [8.578039, 53.541939]
+	},
+	"type": "Feature",
+	"id": "Bremerhaven"
+	},
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [9.993333, 53.550556]
+	},
+	"type": "Feature",
+	"id": "Hamburg"
+	},
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [8.694067, 53.8617]
+	},
+	"type": "Feature",
+	"id": "Cuxhaven"
+	},
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [9.473678, 53.599014]
+	},
+	"type": "Feature",
+	"id": "Stade"
+	},
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [10.414444, 53.2525]
+	},
+	"type": "Feature",
+	"id": "Lüneburg"
+	},
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [8.65, 49.866667]
+	},
+	"type": "Feature",
+	"id": "Darmstadt"
+	},		
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [8.682222, 50.110556]
+	},
+	"type": "Feature",
+	"id": "Frankfurt am Main"
+	},		
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [9.6775, 50.552778]
+	},
+	"type": "Feature",
+	"id": "Fulda"
+	},		
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [8.669722, 50.587222]
+	},
+	"type": "Feature",
+	"id": "Gießen"
+	},		
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [8.916667, 50.133333]
+	},
+	"type": "Feature",
+	"id": "Hanau"
+	},		
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [9.5, 51.316667]
+	},
+	"type": "Feature",
+	"id": "Kassel"
+	},		
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [8.772222, 50.8075]
+	},
+	"type": "Feature",
+	"id": "Marburg"
+	},		
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [8.770833, 50.099444]
+	},
+	"type": "Feature",
+	"id": "Offenbach am Main"
+	},		
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [8.504444, 50.557778]
+	},
+	"type": "Feature",
+	"id": "Wetzlar"
+	},		
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [8.241377, 50.082054]
+	},
+	"type": "Feature",
+	"id": "Wiesbaden"
+	},		
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [9.706703, 50.868575]
+	},
+	"type": "Feature",
+	"id": "Bad Hersfeld"
+	},		
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [8.755, 50.335278]
+	},
+	"type": "Feature",
+	"id": "Friedberg (Hessen)"
+	},		
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [8.75, 50.366667]
+	},
+	"type": "Feature",
+	"id": "Bad Nauheim"
+	},		
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [8.066667, 50.383333]
+	},
+	"type": "Feature",
+	"id": "Limburg an der Lahn"
+	},		
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [8.411944, 49.995]
+	},
+	"type": "Feature",
+	"id": "Rüsselsheim"
+	},		
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [13.261, 53.557]
+	},
+	"type": "Feature",
+	"id": "Neubrandenburg"
+	},		
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [12.133333, 54.083333]
+	},
+	"type": "Feature",
+	"id": "Rostock"
+	},		
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [11.416667, 53.633333]
+	},
+	"type": "Feature",
+	"id": "Schwerin"
+	},		
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [13.089722, 54.313889]
+	},
+	"type": "Feature",
+	"id": "Stralsund"
+	},		
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [13.381111, 54.095833]
+	},
+	"type": "Feature",
+	"id": "Greifswald"
+	},		
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [11.462017, 53.888214]
+	},
+	"type": "Feature",
+	"id": "Wismar"
+	},		
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [10.080556, 52.626111]
+	},
+	"type": "Feature",
+	"id": "Celle"
+	},		
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [9.935556, 51.533889]
+	},
+	"type": "Feature",
+	"id": "Göttingen"
+	},		
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [9.738611, 52.374444]
+	},
+	"type": "Feature",
+	"id": "Hannover"
+	},		
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [9.95, 52.15]
+	},
+	"type": "Feature",
+	"id": "Hildesheim"
+	},		
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [8.213889, 53.143889]
+	},
+	"type": "Feature",
+	"id": "Oldenburg"
+	},		
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [8.043056, 52.278889]
+	},
+	"type": "Feature",
+	"id": "Osnabrück"
+	},		
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [10.406111, 52.153333]
+	},
+	"type": "Feature",
+	"id": "Salzgitter"
+	},		
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [10.521111, 52.269167]
+	},
+	"type": "Feature",
+	"id": "Braunschweig"
+	},		
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [10.787222, 52.423056]
+	},
+	"type": "Feature",
+	"id": "Wolfsburg"
+	},		
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [8.1075, 53.521944]
+	},
+	"type": "Feature",
+	"id": "Wilhelmshaven"
+	},		
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [8.633889, 53.049722]
+	},
+	"type": "Feature",
+	"id": "Delmenhorst"
+	},		
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [7.205556, 53.366944]
+	},
+	"type": "Feature",
+	"id": "Emden"
+	},		
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [9.36, 52.103056]
+	},
+	"type": "Feature",
+	"id": "Hameln"
+	},		
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [9.74, 52.439444]
+	},
+	"type": "Feature",
+	"id": "Langenhagen"
+	},		
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [7.317222, 52.523333]
+	},
+	"type": "Feature",
+	"id": "Lingen (Ems)"
+	},		
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [7.070556, 52.436111]
+	},
+	"type": "Feature",
+	"id": "Nordhorn"
+	},		
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [6.083611, 50.776667]
+	},
+	"type": "Feature",
+	"id": "Aachen"
+	},		
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [8.532222, 52.020417]
+	},
+	"type": "Feature",
+	"id": "Bielefeld"
+	},		
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [7.216944, 51.4825]
+	},
+	"type": "Feature",
+	"id": "Bochum"
+	},		
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [7.099814, 50.733992]
+	},
+	"type": "Feature",
+	"id": "Bonn"
+	},		
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [7.463889, 51.514167]
+	},
+	"type": "Feature",
+	"id": "Dortmund"
+	},		
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [6.782778, 51.225556]
+	},
+	"type": "Feature",
+	"id": "Düsseldorf"
+	},		
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [6.762692, 51.435147]
+	},
+	"type": "Feature",
+	"id": "Duisburg"
+	},		
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [7.014761, 51.458069]
+	},
+	"type": "Feature",
+	"id": "Essen"
+	},		
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [7.475, 51.359444]
+	},
+	"type": "Feature",
+	"id": "Hagen"
+	},		
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [6.956944, 50.938056]
+	},
+	"type": "Feature",
+	"id": "Köln"
+	},		
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [6.564167, 51.336944]
+	},
+	"type": "Feature",
+	"id": "Krefeld"
+	},		
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [6.441944, 51.191111]
+	},
+	"type": "Feature",
+	"id": "Mönchengladbach"
+	},		
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [7.628694, 51.962944]
+	},
+	"type": "Feature",
+	"id": "Münster (Westfalen)"
+	},		
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [8.757222, 51.719444]
+	},
+	"type": "Feature",
+	"id": "Paderborn"
+	},		
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [8.024444, 50.874444]
+	},
+	"type": "Feature",
+	"id": "Siegen"
+	},		
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [7.211111, 51.259167]
+	},
+	"type": "Feature",
+	"id": "Wuppertal"
+	},		
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [7.768889, 49.444722]
+	},
+	"type": "Feature",
+	"id": "Kaiserslautern"
+	},		
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [7.593889, 50.356667]
+	},
+	"type": "Feature",
+	"id": "Koblenz"
+	},		
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [8.271111, 50]
+	},
+	"type": "Feature",
+	"id": "Mainz"
+	},		
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [6.6439, 49.7596]
+	},
+	"type": "Feature",
+	"id": "Trier"
+	},		
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [7.866944, 49.846944]
+	},
+	"type": "Feature",
+	"id": "Bad Kreuznach"
+	},		
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [8.123056, 49.199444]
+	},
+	"type": "Feature",
+	"id": "Landau in der Pfalz"
+	},		
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [8.434167, 49.317222]
+	},
+	"type": "Feature",
+	"id": "Speyer"
+	},		
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [8.360833, 49.633056]
+	},
+	"type": "Feature",
+	"id": "Worms"
+	},		
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [7.360833, 49.249444]
+	},
+	"type": "Feature",
+	"id": "Zweibrücken"
+	},		
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [7.333333, 49.316667]
+	},
+	"type": "Feature",
+	"id": "Homburg"
+	},		
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [7, 49.233333]
+	},
+	"type": "Feature",
+	"id": "Saarbrücken"
+	},		
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [12.924167, 50.832222]
+	},
+	"type": "Feature",
+	"id": "Chemnitz"
+	},		
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [13.73836, 51.049259]
+	},
+	"type": "Feature",
+	"id": "Dresden"
+	},		
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [12.37475, 51.340333]
+	},
+	"type": "Feature",
+	"id": "Leipzig"
+	},		
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [14.423889, 51.181389]
+	},
+	"type": "Feature",
+	"id": "Bautzen"
+	},		
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [14.987222, 51.152778]
+	},
+	"type": "Feature",
+	"id": "Görlitz"
+	},		
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [14.25, 51.43333]
+	},
+	"type": "Feature",
+	"id": "Hoyerswerda"
+	},		
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [12.138333, 50.495]
+	},
+	"type": "Feature",
+	"id": "Plauen"
+	},		
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [12.492222, 50.718889]
+	},
+	"type": "Feature",
+	"id": "Zwickau"
+	},		
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [11.97, 51.482778]
+	},
+	"type": "Feature",
+	"id": "Halle (Saale)"
+	},		
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [12.246944, 51.860556]
+	},
+	"type": "Feature",
+	"id": "Dessau-Roßlau"
+	},		
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [11.616667, 52.133333]
+	},
+	"type": "Feature",
+	"id": "Magdeburg"
+	},		
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [11.056667, 51.898056]
+	},
+	"type": "Feature",
+	"id": "Halberstadt"
+	},		
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [11.86, 52.605278]
+	},
+	"type": "Feature",
+	"id": "Stendal"
+	},		
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [9.436667, 54.781944]
+	},
+	"type": "Feature",
+	"id": "Flensburg"
+	},		
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [10.140556, 54.325278]
+	},
+	"type": "Feature",
+	"id": "Kiel"
+	},		
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [10.686389, 53.869722]
+	},
+	"type": "Feature",
+	"id": "Lübeck"
+	},		
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [9.99, 54.071389]
+	},
+	"type": "Feature",
+	"id": "Neumünster"
+	},		
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [11.029167, 50.978056]
+	},
+	"type": "Feature",
+	"id": "Erfurt"
+	},		
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [12.083333, 50.880556]
+	},
+	"type": "Feature",
+	"id": "Gera"
+	},		
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [11.586361, 50.927206]
+	},
+	"type": "Feature",
+	"id": "Jena"
+	},		
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [12.433333, 50.985]
+	},
+	"type": "Feature",
+	"id": "Altenburg"
+	},		
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [10.324556, 50.974583]
+	},
+	"type": "Feature",
+	"id": "Eisenach"
+	},		
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [10.718333, 50.948889]
+	},
+	"type": "Feature",
+	"id": "Gotha"
+	},		
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [10.791389, 51.500278]
+	},
+	"type": "Feature",
+	"id": "Nordhausen"
+	},		
+	{
+		"geometry": {
+		"type": "Point",
+		"coordinates": [11.329722, 50.979444]
+	},
+	"type": "Feature",
+	"id": "Weimar"
 	}
-]
+	]
 };
